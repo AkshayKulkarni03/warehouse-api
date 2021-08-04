5d35be05a8489b37eaf006f855300dd23c4c17e3
Akshay Kulkarni <akshay.kulkarni8112@gmail.com>
2021-08-04 23:54:03 +0200

    added controller

---
+++ src/main/java/com/ikea/assignment/warehouse/api/controller/WarehouseController.java
@@ -0,0 +1,19 @@
+package com.ikea.assignment.warehouse.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(path = "/warehouses")
+public class WarehouseController {
+
+    @PostMapping(path = "/inventory", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> loadInventory(MultipartFile file) {
+        return ResponseEntity.ok("done");
+    }
+}
